a04c91f963166ed4220750b0012e9bfc2cdbdfa1
Wen Hao <wennho@gmail.com>
2013-09-02 22:17:05 -0700

    finally works

--- ads/craigs_view.py
+++ ads/craigs_view.py
@@ -1,45 +1,44 @@
 from django.views import generic
-from ads.models import ScrapedAd
 
-
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
+from multiprocessing import Process, Queue
+from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from scrapy import log, signals
+from scrapy import log
 from scraper.scraper.spiders.craigslist_spider import CraigslistSpider
-from scrapy.xlib.pydispatch import dispatcher
 
-def stop_reactor():
-    reactor.stop()
 
-class CraigsView(generic.ListView):
+class CraigsView( generic.ListView ):
     template_name = 'ads/scraped_index.html'
     context_object_name = 'scraped_ad_list'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data( self, **kwargs ):
         # Call the base implementation first to get a context
-        context = super(CraigsView, self).get_context_data(**kwargs)
+        context = super( CraigsView, self ).get_context_data( **kwargs )
         if 'q' in self.request.GET:
             context['search'] = self.request.GET['q']
         return context
 
 
-    def get_queryset(self):
-        spider = CraigslistSpider()
-        crawler = Crawler(Settings())
-        crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-        crawler.configure()
-        crawler.crawl(spider)
-        crawler.start()
-        log.start(loglevel=log.DEBUG)
-        log.msg('Running reactor...')
-        reactor.run(installSignalHandlers=0)  # the script will block here until the spider is closed
-        log.msg('Reactor stopped.')
+    def get_queryset( self ):
+        return crawler.crawl()
 
+class CrawlerScript():
+    def __init__( self ):
+        self.crawler = CrawlerProcess( Settings() )
+        self.crawler.install()
+        self.crawler.configure()
+    def _crawl( self, queue ):
+        log.start( loglevel = log.DEBUG )
+        current_spider = CraigslistSpider()
+        self.crawler.crawl( current_spider )
+        self.crawler.start()
+        self.crawler.stop()
+        queue.put( current_spider.get_object_list() )
+    def crawl( self ):
+        q = Queue()
+        p = Process( target = self._crawl, args = ( q, ) )
+        p.start()
+        p.join()
+        return q.get()
 
-
-        if 'q' in self.request.GET:
-            search_words = self.request.GET['q'].strip().split()
-            for word in search_words:
-                queryset = queryset.filter(title__icontains=word)
-        return spider.get_object_list()
+crawler = CrawlerScript()
